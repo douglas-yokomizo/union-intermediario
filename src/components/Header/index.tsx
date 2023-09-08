fef1af73e853541d3fb7e71bf32ea599d032a9d6
Marcelo Ramalho Oliveira <skalinichenko@gmail.com>
2023-09-08 20:35:40 -0300

    adicionando onchange input busca

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react'
 import styles from './header.module.scss'
 
 const Header = () => {
+
+  const aoDigitado = (evento) => {        
+    let valor = evento.target.value;
+    console.log(evento.target.value);
+}
+
   return (
     <div className={styles.header}>
       <h1>Users List</h1>
-      <input type="text" placeholder="Search user..." />
+      
+      <input type="text" onChange={aoDigitado} placeholder="Search user..." />
+
       <div className={styles.divider}></div>
     </div>
   )
